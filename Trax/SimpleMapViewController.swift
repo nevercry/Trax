324d8334b7ea150d1401a00eb53e9707541dfca3
nevercry <ysfnevercry@gmail.com>
2015-10-16 22:55:26 +0800

    Finish Lecture16

---
+++ Trax/SimpleMapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMapViewController.swift
+//  Trax
+//
+//  Created by nevercry on 10/16/15.
+//  Copyright © 2015 nevercry. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class SimpleMapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+}
